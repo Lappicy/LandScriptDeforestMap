0ea29cbf065e17d38d6e5206f25aaf736f7c3a11
Lappicy <lappicy@gmail.com>
2026-06-18 20:57:44 -0300

    Add files via upload

--- Example application/Example.R
+++ Example application/Example.R
@@ -1,29 +1,26 @@
-# Read data from package ####
+# Read data from package
 example.files()
 
 
 # Caverna do Maroaga (Presidente Figueiredo) example ####
-FinalAnalysis <- Growth.Analysis(geo.file = CavernaMaroaga,
-                                 tif.folder = MapBiomas_8_example,
-                                 mesh.size = 0.045,
-                                 output.folder = "Results/",
-                                 output.name = "Analysis_CavernaMaroaga",
-                                 MAPBIOMAS = 8)
+FinalAnalysis <-
+  Growth.Analysis(geo.file = CavernaMaroaga,
+                  tif.folder = MapBiomas_8_example,
+                  mesh.size = 0.045,
+                  output.folder = "Results/",
+                  output.name = "Analysis_CavernaMaroaga",
+                  MAPBIOMAS = 8)
 
 
 # Graph number 1 (time series + correlation) ####
-graphical.timeseries(proxy.table = FinalAnalysis,
-                     comparison.names = c("Growth_Agriculture", "Growth_Mining",
-                                          "Growth_Pasture", "Growth_Urban"),
-                     comparison.color = c("darkorange", "grey50",  "#EA9999", "purple"),
+gg.deforestation.cor(proxy.table = FinalAnalysis,
+                     comparison.name = "Growth",
+                     comparison.color = c("purple", "grey50", "#EA9999", "darkorange"),
                      save.as = "Results/Maroaga Deforestation vs Growth.png",
                      title.name = "Analysis for Caverna do Maroaga")
 
 
 # Graph number 2 (mesh map) ####
-map.layout(mesh.data = FinalAnalysis,
-           class = "Deforestation",
-           year.used = "all",
-           col.limits = c(0, 1, 2, 5),
-           save.map.as = "Results/Map acumulated deforestation.png")
-
+mesh.map(mesh.data = FinalAnalysis,
+         save.map.as = "Results/Map acumulated deforestation.png",
+         map.height = 2300, map.width = 2100, map.units = "px")
